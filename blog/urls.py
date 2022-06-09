566d199217d137752bf6053cd7ced25ea6f90c26
MAXI8594 <molivera154@alumnos.iu.edu.ar>
2022-06-08 21:15:21 -0300

    55

---
+++ blog/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from .views import render_post ,post_detail
+
+urlpatterns = [
+    path('', render_post , name='post'),
+    path ('<int:post_id>' , post_detail ),
+
+]
+
+
+
---
+++ blog/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render , get_object_or_404
+from .models import Post
+
+# Create your views here.
+def render_post(request):
+    post = Post.objects.all()
+
+
+    return render (request , 'post.html' ,{ 'post': post })
+
+def post_detail(request ,post_id):
+    get_object_or_404(Post , pk=post_id)
+    return render (request ,'post_details.html', {'posts': post_id} )
