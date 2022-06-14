c0a7992a120adcc6e0d4f59b4ebaff1b666a98e8
MAXI8594 <molivera154@alumnos.iu.edu.ar>
2022-06-14 00:53:18 -0300

    ll

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from .views import render_post ,post_detail
 
+  
+
 urlpatterns = [
     path('', render_post , name='post'),
     path ('<int:post_id>' , post_detail ),
