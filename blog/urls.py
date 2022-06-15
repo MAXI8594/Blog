83b86f3b9f3bf345f54df4716604db46cdbd75ac
MAXI8594 <molivera154@alumnos.iu.edu.ar>
2022-06-15 03:00:43 -0300

    rr

--- blog/urls.py
+++ blog/urls.py
@@ -2,9 +2,13 @@ from django.urls import path
 from . import views
 from .views import render_posts , post_detail 
 
+app_name= 'blog'
 
 urlpatterns = [
     path("", render_posts , name='post'),
-    path("<int:post_id>", post_detail)]
+    path("<int:post_id>", post_detail ,name='post_details'),]
+
+
+
 
 
